2e9f0b4da46b79d7c493e316e3e749e0f1f41f97
Kaialan <satyajit1501@gmail.com>
2024-04-05 21:05:46 +0530

    new ui for home page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,16 @@
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-center items-center h-[50vh] w-full bg-[#1f164b]">
-      <div>
-        This is footer baby
+    <footer className="flex flex-col md:flex-row justify-center gap-3 md:justify-between items-center h-20 w-full bg-black md:px-32 absolute bottom-0 left-0 z-30">
+
+      <div className="">
+        <p>Chit-Chat 2024. All rights reserved</p>
+      </div>
+      <div className="flex justify-between items-center gap-10 font-bold cursor-wait">
+        <p>Docs</p>
+        <p>Github</p>
+        <p>Twitter</p>
+        <p>Discord</p>
       </div>
     </footer>
   )
